5d26ccd75f4467015c378e200e43025ac84ebbc1
kronographTL <kronographtl@gmail.com>
2020-08-18 23:25:26 +0800

    Initial commit

---
+++ src/main/java/com/ms/stocks/publisher/StocksPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.ms.stocks.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/stocks/publisher/StocksPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.stocks.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
